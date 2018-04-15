b2932b3b2aa0668fc9cdb5fea36dda3b3e615b52
terrycojones <terry@jon.es>
2018-04-15 22:16:53 +0100

    Added error script back into specification seeing as that now works on csd3

--- 07-error/error.sh
+++ 07-error/error.sh
@@ -11,7 +11,4 @@ echo "  ERROR!! SLURM pipeline finished at `date`" >> $log
 echo "  Creating $errorFile." >> $log
 touch $errorFile
 
-echo "  Removing $runningFile and $doneFile." >> $log
-rm -f $runningFile $doneFile
-
 logStepStop $log
